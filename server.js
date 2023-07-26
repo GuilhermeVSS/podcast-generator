6dabe8838044b744aacec768eeae6dbb4adc2601
gvss <gvssilva@dcomp.ufs.br>
2023-07-25 22:59:11 -0300

    fix: server directory

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const app = require('./app');
+const app = require('./src/app');
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT,()=>{
-    console.log('listening on port: '+PORT);
+    console.log('listening on port: '+ PORT);
 });
